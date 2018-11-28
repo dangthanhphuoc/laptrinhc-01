42b18afe599b067c3ab055f007cdbcb35066797b
dangthanhphuoc <thanhphuoc2194@gmail.com>
2018-11-28 19:21:50 +0700

    comment

---
+++ bai nhap.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+ int x; // luu gia tri i * j
+ int j=1;
+    for ( int i = 1; i <= 9; i++)
+        {
+            while (j<=9)
+			{x=i*j;
+			cout<<x<<"\t ";
+			j++;}
+             
+             cout <<endl;}
+       
+        }
+  
+		
+	
+	
+	
+	
